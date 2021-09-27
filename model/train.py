139fef4b2033f78be2eab571c771bdef550dd8c8
yehoon17 <yehoon17@gmail.com>
2021-09-27 10:49:52 +0900

    Update train.py
    Add loss function TripletLoss class.

--- model/train.py
+++ model/train.py
@@ -50,3 +50,22 @@ class Faces(Dataset):
             negative_img = self.transform(self.to_pil(negative_img))
 
         return anchor_img, positive_img, negative_img, anchor_label
+
+
+class TripletLoss(nn.Module):
+    def __init__(self, margin = 1.0):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+        
+    def calc_euclidean(self, x1, x2):
+        return (x1-x2).pow(2).sum(1) 
+    
+    def forward(self, anchor: torch.Tensor, positive: torch.Tensor, negative: torch.Tensor):
+        
+        distance_positive = self.calc_euclidean(anchor, positive)
+        distance_negative = self.calc_euclidean(anchor, negative)
+        losses = torch.relu(distance_positive - distance_negative + self.margin)
+
+        return losses.mean()
+
+        

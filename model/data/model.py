f1022e4185dd5f6e2a30494040b803bff288d9be
yehoon17 <yehoon17@gmail.com>
2021-09-27 10:26:00 +0900

    Create model.py
    Import library.

---
+++ model/data/model.py
@@ -0,0 +1,16 @@
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms, datasets
+import torchvision.transforms as transforms
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch import Tensor
+import torch.nn.functional as F
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import random
+from typing import List, Any, Tuple, Optional
